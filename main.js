414689d9176dee59c1e39e6a6af2f28768045b7c
你çclang1996 <>
2020-06-18 14:51:18 +0800

    dom2

---
+++ main.js
@@ -0,0 +1,9 @@
+// const div = dom.create('<div><span>hi</span></div>')
+const div = dom.create("<div>newdiv</div>")
+dom.after(test,div)
+const div3 = dom.create("<div id='parent'></div>")
+dom.wrap(test,div3)
+
+dom.attr(test,'title','hi,二狗')
+const title = dom.attr(test,'title')
+console.log(`title:${title}`)
